4a5f4b730832da1276f5f4f800f3af60345bb617
Anamika <dfdf>
2024-10-07 13:33:30 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let arr = [3,7,15,2,9,20,5,14,12, 1 8, 11, 6,19,4,10, 17,13,16,18]; 
+for (let i=0; i<arr.length; i++){
+      if (i%2 == 0){
+        console.log("even")
+      }
+      else {
+        console.log("odd")
+      }
+
+}
